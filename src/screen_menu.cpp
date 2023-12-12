1a4e88967ff677167cf37b70e34e6ad31fa8e954
Brandon Zobernig <brandon.zm02@gmail.com>
2023-12-12 11:58:46 +0100

    New Menu Screen

--- src/screen_menu.cpp
+++ src/screen_menu.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 
 void screen_menu(){
-    DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 200, RED);
-    DrawText("Press SPACE To Start", GetScreenWidth() / 4 + 70, GetScreenHeight() / 2 - 12.5, 30, WHITE);
+    DrawCircle(GetScreenWidth() / 2 , GetScreenHeight() / 2, 150, RED);
+    DrawText("Press SPACE", GetScreenWidth() / 3 + 55, GetScreenHeight() / 2 - 35, 30, WHITE);
+    DrawText("to start", GetScreenWidth() / 3 + 90, GetScreenHeight() / 2 + 5, 30, WHITE);
+    DrawText("CoolesGame24", GetScreenWidth() / 3 - 20, GetScreenHeight() / 20, 50,GREEN);
 }
