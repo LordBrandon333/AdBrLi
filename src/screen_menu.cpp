66b180b1088340858eb4f697949d563efd9fb119
Brandon Zobernig <brandon.zm02@gmail.com>
2023-12-12 11:48:16 +0100

    New Menu Screen

--- src/screen_menu.cpp
+++ src/screen_menu.cpp
@@ -2,5 +2,5 @@
 
 void screen_menu(){
     DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 200, RED);
-    DrawText("Press Space To Start", GetScreenWidth() / 4 + 70, GetScreenHeight() / 2 - 12.5, 30, WHITE);
+    DrawText("Press SPACE To Start", GetScreenWidth() / 4 + 70, GetScreenHeight() / 2 - 12.5, 30, WHITE);
 }
